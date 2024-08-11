83c1eb667d4dfb44676f66a4381502ded9e56040
krishan <krishanmondal273@gmail.com>
2024-08-11 14:18:14 +0530

    Improve search functionality

--- src/components/Utility.jsx
+++ src/components/Utility.jsx
@@ -119,17 +119,17 @@ export const formatDateToYYYYMMM = () => {
 
 export const searchData = (data) => {
   if (data === "") return [];
-
   const state = store.getState();
   const usersData = state.usersData;
   const startsWithData = usersData.filter(
     (user) =>
-      user.name.toLowerCase().startsWith(data) ||
-      user.userName.toLowerCase().startsWith(data)
+      user.name.toLowerCase().startsWith(data.toLowerCase()) ||
+      user.userName.toLowerCase().startsWith(data.toLowerCase())
   );
   const containsData = usersData.filter(
     (user) =>
-      !startsWithData.includes(user) && user.name.toLowerCase().includes(data)
+      !startsWithData.includes(user) &&
+      user.name.toLowerCase().includes(data.toLowerCase())
   );
   return [...startsWithData, ...containsData];
 };
